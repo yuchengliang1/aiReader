bf264871d9a5cc7dc7b8e4241de42ae92184a54e
yuchengliang <2232166515@qq.com>
2026-01-19 11:06:40 +0800

    first commit

---
+++ app/src/main/java/io/legado/app/ui/widget/recycler/NoChildScrollLinearLayoutManager.kt
@@ -0,0 +1,36 @@
+package io.legado.app.ui.widget.recycler
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class NoChildScrollLinearLayoutManager @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : LinearLayoutManager(context, attrs, defStyleAttr, defStyleRes) {
+
+    override fun requestChildRectangleOnScreen(
+        parent: RecyclerView,
+        child: View,
+        rect: Rect,
+        immediate: Boolean,
+        focusedChildVisible: Boolean
+    ): Boolean {
+        return false
+    }
+
+    override fun requestChildRectangleOnScreen(
+        parent: RecyclerView,
+        child: View,
+        rect: Rect,
+        immediate: Boolean
+    ): Boolean {
+        return false
+    }
+
+}
